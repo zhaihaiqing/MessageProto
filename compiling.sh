d1295e73c605574c647dd38e0de969bd00b947a0
songxiaopeng <songxp@smartbow.net>
2019-05-20 11:25:29 +0800

    feat: 增加Ack消息定义，更新上行传感类型消息定义 --user=宋晓鹏　--task=1003186

--- compiling.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -rf $PWD/compiling/*
-
-echo "clear all path-compiling files"
-
-protoc --proto_path=$PWD --cpp_out=$PWD/compiling $PWD/SensorUpMessage.proto $PWD/SensorMessages/SensorMessages.proto
-
-echo "output success"
