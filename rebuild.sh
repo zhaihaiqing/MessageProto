61714b04d64ff967ff1c48b9944303fc07e673a2
songxiaopeng <songxp@smartbow.net>
2020-10-23 14:52:56 +0800

    update: update ci --user=songxp

--- rebuild.sh
+++ rebuild.sh
@@ -1,13 +1,12 @@
 COMPILE_PATH=.Rebuild
-BRANCH_NAME=`git branch |awk '{print $2}'`
-echo "current branch:${BRANCH_NAME}"
+echo "current branch:${CI_COMMIT_REF_NAME}"
 
 rm -rf ${COMPILE_PATH}
 mkdir ${COMPILE_PATH}
 cd ${COMPILE_PATH}
 # 下载MessageProto
 echo "step1: clone MessageProto..."
-git clone -b ${BRANCH_NAME} http://git.thunics.org/dp/MessageProto.git
+git clone -b ${CI_COMMIT_REF_NAME} http://git.thunics.org/dp/MessageProto.git
 # 下载wsn-data
 echo "step2: clone wsn-data..."
 git clone -b master http://git.thunics.org/embedded/wsn-data.git
