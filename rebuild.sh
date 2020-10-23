2c830b00d1262ab580242dfff6afb7c427593dd3
songxiaopeng <songxp@smartbow.net>
2020-10-23 14:37:17 +0800

    build: 支持ci校验 --user=songxp

---
+++ rebuild.sh
@@ -0,0 +1,30 @@
+COMPILE_PATH=.Rebuild
+BRANCH_NAME=`git branch |awk '{print $2}'`
+echo "current branch:${BRANCH_NAME}"
+
+rm -rf ${COMPILE_PATH}
+mkdir ${COMPILE_PATH}
+cd ${COMPILE_PATH}
+# 下载MessageProto
+echo "step1: clone MessageProto..."
+git clone -b ${BRANCH_NAME} http://git.thunics.org/dp/MessageProto.git
+# 下载wsn-data
+echo "step2: clone wsn-data..."
+git clone -b master http://git.thunics.org/embedded/wsn-data.git
+
+# 校验
+echo "step3: check MessageProto"
+../CompileMessageProto/run.sh > result.txt
+
+# 更新git仓库
+RESULT=`awk '$1 ~ /congratulation/ {print "yes"}' result.txt`
+if [ "${RESULT}" == "yes" ]; then
+    echo "MessageProto check success"
+    cd MessageProto/
+    git push
+    exit 0
+else
+  echo "MessageProto check failed"
+  cat result.txt
+  exit 1
+fi
