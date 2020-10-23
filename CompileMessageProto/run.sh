5ca7b5ca9320eada6a06ad6759cfc05253789977
songxiaopeng <songxp@smartbow.net>
2020-10-23 16:27:30 +0800

    update: update ci --user=songxp

--- CompileMessageProto/run.sh
+++ CompileMessageProto/run.sh
@@ -2,10 +2,7 @@
 
 PROGRAM_VERSION=v1.2.1
 
-docker rm BStack2Proto3
-docker rmi docker-registry.thunics.org:5000/bstack2proto3:${PROGRAM_VERSION}
-
 mkdir .output
 docker pull docker-registry.thunics.org:5000/bstack2proto3:${PROGRAM_VERSION}
-docker run --name BStack2Proto3 -v $PWD/MessageProto:/home/work/bstack2proto3/MessageProto -v $PWD/wsn-data:/home/work/bstack2proto3/wsn-data -v $PWD/.output:/home/work/bstack2proto3/output docker-registry.thunics.org:5000/bstack2proto3:${PROGRAM_VERSION} /home/work/bstack2proto3/BStack2Proto3.sh
+docker run --rm -v $PWD/MessageProto:/home/work/bstack2proto3/MessageProto -v $PWD/wsn-data:/home/work/bstack2proto3/wsn-data -v $PWD/.output:/home/work/bstack2proto3/output docker-registry.thunics.org:5000/bstack2proto3:${PROGRAM_VERSION} /home/work/bstack2proto3/BStack2Proto3.sh
 
